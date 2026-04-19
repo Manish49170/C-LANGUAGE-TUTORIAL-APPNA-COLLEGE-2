79e4dd6c743331003ab6ffc9a99d89a47e3558b6
Manish49170 <kumarm49170@gmail.com>
2026-04-19 14:47:35 +0530

    added file

---
+++ comments.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+  // Comments
+
+  //  singlr line comments
+ // int age =24;
+  // float pi =3.14;
+    // char hastag ='#';
+
+  /* multi-line comments*/
+  /* int age =24;
+   float pi =3.14;
+     char hastag ='#';*/
+
+  return 0;
+}
---
+++ data_type_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+
+  // Data Types
+  int age =24;
+  float pi =3.14;
+  char hastag ='#';
+  return 0;
+}
