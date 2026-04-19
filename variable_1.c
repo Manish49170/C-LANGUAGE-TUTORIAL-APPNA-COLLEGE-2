f5c0333bc86832e0a2e2f0a6a9d338c085c3bdec
Manish49170 <kumarm49170@gmail.com>
2026-04-19 10:11:25 +0530

    added file

---
+++ variable_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int number = 2;
+  char star = 'x';
+  int age = 22;
+  age = 24;
+  float pi =3.14;
+
+  // case sensitive 
+  int a = 40;
+  int A = 40;
+
+  //first char alphabet or ' _'
+  int _age =22;
+
+// no comma/ blank space
+int final_price = 100;
+  return 0;
+}
